1f55e2b23cc4c024ad32ca53f60bb2dbb275f8bd
Vladyslav_Pyrozhenko <vladyslav22pyrozhenko@gail.com>
2022-07-26 18:39:59 +0300

    add dropdown menu

---
+++ client/src/components/DropdownMenu.tsx
@@ -0,0 +1,143 @@
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import Menu, { MenuProps } from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Divider from "@mui/material/Divider";
+import {
+  AnnouncementOutlined,
+  AutoAwesome,
+  BorderColorOutlined,
+  ClosedCaption,
+  DashboardOutlined,
+  FullscreenOutlined,
+  MoreVertOutlined,
+  PhoneForwardedRounded,
+  QueryStatsOutlined,
+  RadioButtonCheckedOutlined,
+  ReportGmailerrorredOutlined,
+  SettingsOutlined,
+} from "@mui/icons-material";
+
+const StyledMenu = styled((props: MenuProps) => (
+  <Menu
+    style={{
+      left: "410px",
+      top: "-70px",
+    }}
+    elevation={0}
+    anchorOrigin={{
+      vertical: "top",
+      horizontal: "left",
+    }}
+    transformOrigin={{
+      vertical: "top",
+      horizontal: "right",
+    }}
+    {...props}
+  />
+))(({ theme }) => ({
+  "& .MuiPaper-root": {
+    borderRadius: 6,
+    marginTop: theme.spacing(0),
+    minWidth: 200,
+    color:
+      theme.palette.mode === "light"
+        ? "rgb(55, 65, 81)"
+        : theme.palette.grey[300],
+    boxShadow:
+      "rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
+    "& .MuiMenu-list": {
+      padding: "4px 0",
+    },
+    "& .MuiMenuItem-root": {
+      "& .MuiSvgIcon-root": {
+        fontSize: 18,
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(1),
+      },
+      "&:active": {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          theme.palette.action.selectedOpacity
+        ),
+      },
+    },
+  },
+}));
+
+const DropdownMenu = () => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <div onClick={handleClick}>
+        <MoreVertOutlined />
+      </div>
+      <StyledMenu
+        id="demo-customized-menu"
+        MenuListProps={{
+          "aria-labelledby": "demo-customized-button",
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose} disableRipple>
+          <BorderColorOutlined />
+          Дошка для конференцій
+        </MenuItem>
+        <MenuItem onClick={handleClose} disableRipple>
+          <RadioButtonCheckedOutlined />
+          Записувати зустріч
+        </MenuItem>
+        <Divider sx={{ my: 0.5 }} />
+        <MenuItem onClick={handleClose} disableRipple>
+          <DashboardOutlined />
+          Змінити макет
+        </MenuItem>
+        <MenuItem onClick={handleClose} disableRipple>
+          <FullscreenOutlined />
+          Повноекранний режим
+        </MenuItem>
+        <MenuItem onClick={handleClose} disableRipple>
+          <AutoAwesome />
+          Застосувати візуальні ефекти
+        </MenuItem>
+        <MenuItem onClick={handleClose} disableRipple>
+          <ClosedCaption />
+          Увімкнути субтитри
+        </MenuItem>
+        <MenuItem onClick={handleClose} disableRipple>
+          <PhoneForwardedRounded />
+          Використовувати телефон для передачі зв'язку
+        </MenuItem>
+        <Divider sx={{ my: 0.5 }} />
+        <MenuItem onClick={handleClose} disableRipple>
+          <AnnouncementOutlined />
+          Повідомити про проблему
+        </MenuItem>
+        <MenuItem onClick={handleClose} disableRipple>
+          <ReportGmailerrorredOutlined />
+          Повідомити про порушення
+        </MenuItem>
+        <MenuItem onClick={handleClose} disableRipple>
+          <QueryStatsOutlined />
+          Вирішення проблема і довідка
+        </MenuItem>
+        <MenuItem onClick={handleClose} disableRipple>
+          <SettingsOutlined />
+          Налаштування
+        </MenuItem>
+      </StyledMenu>
+    </div>
+  );
+};
+
+export default DropdownMenu;
--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -3,16 +3,32 @@ import moment from "moment";
 import "moment/locale/uk";
 import "../styles/Footer.css";
 import { useEffect, useState } from "react";
-import { CallEndRounded,  InfoOutlined,  InterestsOutlined,  LockOutlined,  MessageOutlined,  MicOutlined, MoreVertOutlined, PanToolOutlined, PeopleOutlined, PresentToAll, VideocamOutlined } from "@mui/icons-material";
+import {
+  CallEndRounded,
+  InfoOutlined,
+  InterestsOutlined,
+  LockOutlined,
+  MessageOutlined,
+  MicOutlined,
+  MoreVertOutlined,
+  PanToolOutlined,
+  PeopleOutlined,
+  PresentToAll,
+  VideocamOutlined,
+} from "@mui/icons-material";
+import DropdownMenu from "./DropdownMenu";
 
 moment.locale("uk");
 
 const Footer = () => {
   const [time, setTime] = useState("");
   useEffect(() => {
-    setInterval(() => {
+    const interval = setInterval(() => {
       setTime(moment().format("LT"));
     }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
 
   return (
@@ -24,19 +40,41 @@ const Footer = () => {
           <div>Meeting</div>
         </div>
         <div className="icons-container">
-          <div className="icons-element"><MicOutlined /> </div>
-          <div className="icons-element"><VideocamOutlined /> </div>
-          <div className="icons-element"><PanToolOutlined/> </div>
-          <div className="icons-element"><PresentToAll/> </div>
-          <div className="icons-element"><MoreVertOutlined/> </div>
-          <div className="icons-element end-call"><CallEndRounded/> </div>
+          <div className="icons-element">
+            <MicOutlined />
+          </div>
+          <div className="icons-element">
+            <VideocamOutlined />
+          </div>
+          <div className="icons-element">
+            <PanToolOutlined />
+          </div>
+          <div className="icons-element">
+            <PresentToAll />
+          </div>
+          <div className="icons-element">
+            <DropdownMenu />
+          </div>
+          <div className="icons-element end-call">
+            <CallEndRounded />
+          </div>
         </div>
         <div className="icons-container">
-        <div className="additional-icons"> <InfoOutlined/></div>
-        <div className="additional-icons"> <PeopleOutlined/> </div>
-        <div className="additional-icons"><MessageOutlined /> </div>
-        <div className="additional-icons"><InterestsOutlined /> </div>
-        <div className="additional-icons"><LockOutlined/> </div>
+          <div className="additional-icons">
+            <InfoOutlined />
+          </div>
+          <div className="additional-icons">
+            <PeopleOutlined />
+          </div>
+          <div className="additional-icons">
+            <MessageOutlined />
+          </div>
+          <div className="additional-icons">
+            <InterestsOutlined />
+          </div>
+          <div className="additional-icons">
+            <LockOutlined />
+          </div>
         </div>
       </Stack>
     </div>
